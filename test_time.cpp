45cc3aff8c416cc212d3dd8503c3eba5371897d0
yaoxisheng <yaoxisheng@yaoxisheng-Lenovo-IdeaPad-Y470.(none)>
2013-06-15 20:27:34 -0400

    add test time version 2

--- test_time.cpp
+++ test_time.cpp
@@ -15,17 +15,18 @@ sVector *get_sVec3();
 vectorSet get_vSet();
 void get_vSet2(vectorSet &temp_vSet);
 vectorSet *get_vSet3();
+vectorSet &get_vSet4();
 
 int main(int argc, char* argv[]){
   Timer t;  
   t.timer();
-  for(int i=0;i<10000;i++){
-    /*
-    vector<float> v,*v2;
-    v = getVec();
-    getVec2(v);
-    v2 = getVec3();
-    delete v2; */
+  for(int i=0;i<100;i++){
+    
+    /* vector<float> v1,v2,*pv;
+    v1 = getVec();
+    getVec2(v2);
+    pv = getVec3();
+    delete pv; */ 
     
     /*
     sVector sVec,*sVec2;
@@ -35,12 +36,13 @@ int main(int argc, char* argv[]){
     delete sVec2;
     */    
     
-    /*
-    vectorSet vSet,*vSet2;
+    /* vectorSet vSet,*vSet2;
     vSet = get_vSet();
     get_vSet2(vSet);
     vSet2 = get_vSet3();
-    delete vSet2;  */
+    delete vSet2;
+    vectorSet &vSet3 = get_vSet4();
+    delete &vSet3; */ 
   }
   t.stopTime();
   cout<<t.getTime()<<endl;
@@ -58,48 +60,57 @@ void getVec2(vector<float> &v){
 }
 
 vector<float> *getVec3(){
-  vector<float> *v = new vector<float>(100000);
+  vector<float> *v = new vector<float>;
   v->assign(100000,2.000);
   return v;
 }
 
 sVector get_sVec(){
   sVector sVec;
-  sVec.v.assign(100000,2.000);
+  sVec.assign(100000,2.000);
   return sVec;
 }
 
 void get_sVec2(sVector &sVec){
-  sVec.v.assign(100000,2.000);  
+  sVec.assign(100000,2.000);  
 }
 
 sVector *get_sVec3(){
   sVector *sVec = new sVector;
-  sVec->v.assign(100000,2.000);
+  sVec->assign(100000,2.000);
   return sVec; 
 }
 
 vectorSet get_vSet(){
   vectorSet temp_vSet;
   sVector temp_sVec;  
-  temp_sVec.v.assign(10000,2.000);
-  list<sVector> temp_list(30,temp_sVec);
+  temp_sVec.assign(10000,2.000);
+  list<sVector> temp_list(3000,temp_sVec);
   temp_vSet.set_vSet(temp_list);
   return temp_vSet;
 }
 
 void get_vSet2(vectorSet &temp_vSet){
   sVector temp_sVec;  
-  temp_sVec.v.assign(10000,2.000);
-  list<sVector> temp_list(30,temp_sVec);
+  temp_sVec.assign(10000,2.000);
+  list<sVector> temp_list(3000,temp_sVec);
   temp_vSet.set_vSet(temp_list);  
 }
 
 vectorSet *get_vSet3(){
   vectorSet *temp_vSet = new vectorSet;
   sVector temp_sVec;  
-  temp_sVec.v.assign(10000,2.000);
-  list<sVector> temp_list(30,temp_sVec);
+  temp_sVec.assign(10000,2.000);
+  list<sVector> temp_list(3000,temp_sVec);
   temp_vSet->set_vSet(temp_list);
   return temp_vSet;
+}
+
+vectorSet &get_vSet4(){
+  vectorSet *temp_vSet = new vectorSet;
+  sVector temp_sVec;  
+  temp_sVec.assign(10000,2.000);
+  list<sVector> temp_list(3000,temp_sVec);
+  temp_vSet->set_vSet(temp_list);
+  return *temp_vSet;
 }
